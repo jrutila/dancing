300266dcf00e399947d729c02cc9dbae701050bf
Juho Rutila <juho.rutila@iki.fi>
2016-01-09 00:36:31 +0200

    Fix wsgi

--- dancing/wsgi.py
+++ dancing/wsgi.py
@@ -8,10 +8,10 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dancing.settings")
+
 from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dancing.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
