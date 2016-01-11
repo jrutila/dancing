1259104b794d5ef24553a6e2c0b847fb13ed46ff
Juho Rutila <juho.rutila@iki.fi>
2016-01-11 09:13:04 +0200

    Supported carousel

--- dancing/static/theme/js/main.js
+++ dancing/static/theme/js/main.js
@@ -16,8 +16,9 @@ $('ul.sf-menu').superfish();
 //================================= Smartum carousel ==============================//
 $("#support").owlCarousel({
    autoPlay: 3200,
-   items : 6,
+   items : 3,
    navigation: false,
+    autoHeight: true,
    itemsDesktop : [1199,5],
    itemsDesktopSmall : [1024,4],
    itemsTablet : [768,3],
