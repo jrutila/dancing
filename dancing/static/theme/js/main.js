81d2d42f8c195a1cfc55001b28e15a993a02f59d
Juho Rutila <juho.rutila@iki.fi>
2016-01-11 02:24:17 +0200

    Add stuff

--- dancing/static/theme/js/main.js
+++ dancing/static/theme/js/main.js
@@ -12,3 +12,16 @@ $(".mainmenu").sticky({topSpacing:0});
 
 $('ul.sf-menu').superfish();
 
+
+//================================= Smartum carousel ==============================//
+$("#support").owlCarousel({
+   autoPlay: 3200,
+   items : 6,
+   navigation: false,
+   itemsDesktop : [1199,5],
+   itemsDesktopSmall : [1024,4],
+   itemsTablet : [768,3],
+   itemsMobile : [500,2],
+   pagination: true
+});
+
