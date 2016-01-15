f52be0c094775f5d4730b4c3ae5f5ef1ed9b98f4
Juho Rutila <juho.rutila@gmail.com>
2016-01-15 14:20:06 +0000

    Activity participation transactions

--- danceclub/models.py
+++ danceclub/models.py
@@ -4,13 +4,19 @@ from datetime import date
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.utils import timezone
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from decimal import *
 
 LEVELS = (('F', 'F'), ('E', 'E'), ('D', 'D'), ('C','C'), ('B','B'), ('A', 'A'))
 AGES = (('L1', 'Lapsi I'), ('L2', 'Lapsi II'), ('J1', 'Juniori I'), ('J2','Juniori II'), ('N','Nuoriso'), ('Y', 'Yleinen'), ('S1', 'Seniori I'),
         ('S2', 'Seniori II'), ('S3', 'Seniori III'), ('S4', 'Seniori IV'))
+        
+season_cost = Decimal("20.00")
+costs = {
+    'dancing': [Decimal("80.00"), Decimal("45.00"), Decimal("35.00")],
+    '*': [Decimal("55.00"), Decimal("45.00"), Decimal("35.00")],
+    }
 
 class Member(models.Model):
     user = models.ForeignKey(User)
@@ -58,6 +64,34 @@ class ActivityParticipation(models.Model):
     def __str__(self):
         return "%s - %s" % (str(self.member), str(self.activity))
         
+@receiver(post_delete, sender=ActivityParticipation)
+def update_transactions(instance, **kwargs):
+    season = Season.objects.get(start__lte=instance.activity.start, end__gte=instance.activity.end)
+    acts = ActivityParticipation.objects.filter(
+        member=instance.member,
+        activity__start__gte=season.start,
+        activity__end__lte=season.end)
+    Transaction.objects.filter(
+        owner=instance.member,
+        source_type=ContentType.objects.get_for_model(Activity),
+        source_id=instance.activity.id).delete()
+        
+    if len(acts) == 0:
+        Transaction.objects.filter(
+            owner=instance.member,
+            source_type=ContentType.objects.get_for_model(season),
+            source_id=season.id)
+    else:
+        trans = Transaction.objects.filter(
+            source_type=ContentType.objects.get_for_model(Activity),
+            source_id__in=[x.activity.id for x in acts])
+        i = 0
+        cost = costs["*"]
+        for t in trans:
+            t.amount = -1*cost[i]
+            if i < len(cost)-1: i = i + 1
+            t.save()
+        
 @receiver(post_save, sender=ActivityParticipation)
 def create_transactions(instance, created, **kwargs):
     if created:
@@ -71,8 +105,24 @@ def create_transactions(instance, created, **kwargs):
             tr = Transaction.objects.create(
                 source=season,
                 owner=instance.member,
-                amount = Decimal('-25.00'),
+                amount = -1*season_cost,
                 title = 'Jäsenmaksu %s' % str(season))
+        try:
+            tr = Transaction.objects.get(
+                source_type=ContentType.objects.get_for_model(instance.activity),
+                source_id=instance.activity.id,
+                owner=instance.member)
+        except Transaction.DoesNotExist:
+            acts = ActivityParticipation.objects.filter(
+                member=instance.member,
+                activity__start__gte=season.start,
+                activity__end__lte=season.end).count()
+            cost = costs["*"][acts-1]
+            tr = Transaction.objects.create(
+                source=instance.activity,
+                owner=instance.member,
+                amount = -1*cost,
+                title = "%s (%s)" % (instance.activity.name, str(season)))
         
 class Transaction(models.Model):
     title = models.CharField(max_length=255, blank=True)
