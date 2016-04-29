fc41ec0fab5863fddf62e476c3e3c88b559fc1bb
Juho Rutila <juho.rutila@gmail.com>
2016-04-29 04:32:57 +0000

    Login required for account redirect

--- common/views.py
+++ common/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from django.views.generic.base import RedirectView
 from danceclub.views import get_member_url
 from danceclub.models import Member
+from django.contrib.auth.decorators import login_required
 
-class ProfileRedirectView(RedirectView):
+class LoginRequiredMixin(object):
+    @classmethod
+    def as_view(cls, **initkwargs):
+        view = super(LoginRequiredMixin, cls).as_view(**initkwargs)
+        return login_required(view)
+
+class ProfileRedirectView(LoginRequiredMixin, RedirectView):
     permanent = False
     def get_redirect_url(self, *args, **kwargs):
         member = Member.objects.get(user=self.request.user)
